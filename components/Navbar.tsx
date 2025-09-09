31a57a88c75d333497acbce80eaf10f5645d4ac7
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-09 14:57:24 +0300

    navbar ui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,35 @@
-export default function Navbar() {
+import Link from 'next/link';
+export default async function Navbar() {
+
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border-b-2 border-gray-200'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex items-center'>
+            <Link href='/' className='flex-shrink-0'>
+              <span className='text-md sm:text-2xl font-bold bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent'>
+                SleepTracker
+              </span>
+            </Link>
+          </div>
+
+          <div className='flex items-center space-x-4'>
+            <Link
+              href='/'
+              className='text-gray-700 hover:text-purple-600 px-2 py-1 rounded-md text-sm sm:px-3 sm:py-2 sm:text-base font-medium hidden sm:block'
+            >
+              Home
+            </Link>
+
+            <Link
+              href='/about'
+              className='text-gray-700 hover:text-purple-600 px-2 py-1 rounded-md text-sm sm:px-3 sm:py-2 sm:text-base font-medium '
+            >
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
