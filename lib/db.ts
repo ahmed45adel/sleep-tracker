9efcf1184571eff43004a229e3a6e95817df1472
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-09 14:54:48 +0300

    navbar empty template

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-// add  no-var
-
-/* eslint-disable no-var */
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
-
-/* eslint-disable no-var */
 
 export const db = globalThis.prisma || new PrismaClient();
 
