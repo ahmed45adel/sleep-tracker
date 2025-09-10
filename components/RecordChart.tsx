2c0f7c202c57d67ae19e81155e841404ad8fd09e
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-10 14:54:23 +0300

    handle empty records

--- components/RecordChart.tsx
+++ components/RecordChart.tsx
@@ -2,7 +2,22 @@ import getRecords from '@/app/actions/getRecords';
 import BarChart from './BarChart';
 
 const RecordChart = async () => {
-  const { records } = await getRecords();
+  const { records} = await getRecords();
+
+  if (!records || records.length === 0) {
+    return (
+      <div className='bg-gray-100 flex items-center justify-center'>
+        <div className='bg-white shadow-lg rounded-lg p-8 w-full  text-center'>
+          <h3 className='text-2xl font-bold mb-4 bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 bg-clip-text text-transparent'>
+            No Sleep Records Found
+          </h3>
+          <p className='text-gray-600'>
+            Start tracking your sleep to see your records here.
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className='bg-gray-100 flex items-center justify-center'>
@@ -11,12 +26,11 @@ const RecordChart = async () => {
           Sleep Records Chart
         </h3>
         <BarChart
-          records={records?.map((record) => ({
+          records={records.map((record) => ({
             ...record,
             date: String(record.date),
           }))}
-        />{' '}
-        {/* Pass records as props */}
+        />
       </div>
     </div>
   );
