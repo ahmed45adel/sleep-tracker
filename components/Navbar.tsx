b22fe6bd705a4458db815b3e324985fbb47ed7f2
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-11 13:42:28 +0300

    white bg for nav and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,8 +6,8 @@ export default async function Navbar() {
   const user = await checkUser();
 
   return (
-    <nav>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 border-b-2 border-gray-200'>
+    <nav className='bg-white'>
+      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex items-center justify-between h-16'>
           <div className='flex items-center'>
             <Link href='/' className='flex-shrink-0'>
