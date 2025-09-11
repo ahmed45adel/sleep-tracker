8dc9301d097fd5ee580e2b9b740371fa4e6be41d
Ahmed Adel <ahmed45adel@yahoo.com>
2025-09-11 12:51:50 +0300

    add best sleep info

--- components/BestWorstSleep.tsx
+++ components/BestWorstSleep.tsx
@@ -1,7 +1,7 @@
 import getBestWorstSleep from '@/app/actions/getBestWorstSleep';
 
 const BestWorstSleep = async () => {
-  const { worstSleep } = await getBestWorstSleep();
+  const { bestSleep, worstSleep } = await getBestWorstSleep();
 
   return (
     <div className='bg-gray-100 flex items-center justify-center'>
@@ -10,6 +10,21 @@ const BestWorstSleep = async () => {
           Best and Worst Sleep
         </h3>
         <div className='flex flex-col sm:flex-row sm:justify-between sm:items-center space-y-6 sm:space-y-0'>
+          {/* Best Sleep */}
+          <div className='text-center'>
+            <h4 className='text-lg font-medium text-gray-600 mb-2'>
+              Best Sleep
+            </h4>
+            <p className='text-3xl font-bold bg-gradient-to-r from-green-500 to-green-700 bg-clip-text text-transparent'>
+              {bestSleep !== undefined
+                ? `${bestSleep} hours`
+                : 'No data available'}
+            </p>
+          </div>
+
+          {/* Divider */}
+          <div className='hidden sm:block h-16 w-px bg-gray-300'></div>
+
           {/* Worst Sleep */}
           <div className='text-center'>
             <h4 className='text-lg font-medium text-gray-600 mb-2'>
